f7e0daa991773ece3a6b00fdb75f48fa421f130e
salmanriazsyed <72282430+salmanriazsyed@users.noreply.github.com>
2021-08-12 21:06:39 +0500

    Menu and Scene Transitions Added

---
+++ Ozzy/Assets/Scripts/QuitToMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitToMenuScript : MonoBehaviour
+{
+    public GameObject areYouSure;
+    public GameObject pauseMenu;
+    public void Yes()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void No()
+    {
+        areYouSure.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+}
---
+++ Ozzy/Assets/Scripts/triggersPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class triggersPlayer : MonoBehaviour
+{
+    private PlayableDirector cutscene;
+    public GameObject cutsceneObject;
+    public GameObject fpsController;
+    public GameObject cutsceneLady;
+    public GameObject nextSceneLoader;
+    private bool isIslandFound = false;
+
+    private void Start()
+    {
+        fpsController.SetActive(true);
+        cutsceneLady.SetActive(false);
+        nextSceneLoader.SetActive(false);
+        cutscene = cutsceneObject.GetComponent<PlayableDirector>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("houseEntry"))
+        {
+            cutsceneLady.SetActive(true);
+            cutscene.Play();
+            fpsController.SetActive(false);
+        }
+
+        if (other.CompareTag("islandFound"))
+        {
+            isIslandFound = true;
+        }
+
+        if (other.CompareTag("underground"))
+        {
+            if (isIslandFound)
+            {
+                fpsController.transform.position = new Vector3(-700, 13, -120);
+            }
+            else
+            {
+                fpsController.transform.position = new Vector3(-740, 15, -350);
+            }
+
+        }
+    }
+}
